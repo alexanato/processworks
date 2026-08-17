5b4562d08ee55b959302d1601560e6251d8f959f
alexanato <79003286+alexanato@users.noreply.github.com>
2026-08-17 16:22:03 +0200

    FallingBlockRendererStateExteension missing

---
+++ src/main/java/at/randorf/processworks/client/renderstate/FallingBlockRenderStateExtension.java
@@ -0,0 +1,12 @@
+package at.randorf.client.renderstate;
+
+import net.minecraft.client.renderer.item.ItemStackRenderState;
+
+public interface FallingBlockRenderStateExtension {
+
+    ItemStackRenderState processworks$getItemStackRenderState();
+
+    int processworks$getLightCoords();
+
+    void processworks$setLightCoords(int lightCoords);
+}
